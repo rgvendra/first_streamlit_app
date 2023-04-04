b66bd32a3664bbd6d68344dfdfcf1401213821a7
Sriram Kalyanaraman <40701783+rgvendra@users.noreply.github.com>
2023-04-04 16:27:43 -0400

    revised as response

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,7 @@ streamlit.text(fruityvice_response)
 #create the repeatable code block (called a function)
 def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    streamlit.text(fruityvice_response.json())
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return  fruityvice_normalized
         
@@ -44,7 +45,6 @@ try:
         streamlit.error("Please select a fruit to get information.")
    else:
         back_from_function = get_fruityvice_data(fruit_choice)
-        streamlit.text(fruityvice_response.json())
         streamlit.dataframe(back_from_function)
 
 except URLError as e:
